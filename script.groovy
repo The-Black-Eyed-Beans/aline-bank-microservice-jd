95871742420edcfd2797ea55472b5fd399cac3e1
joshua-davis1 <joshua.davis@smoothstack.com>
2022-03-21 21:43:06 -0400

    BA-138 patch

--- script.groovy
+++ script.groovy
@@ -2,7 +2,7 @@ def buildApp() {
   sh "git submodule init"
   sh "git submodule update"
   sh "mvn install -DskipTests"
-  sh "cp **/target/*.jar ."
+  sh "cp $DOCKER_IMAGE-microservice/target/*.jar ."
 }
 
 def testApp() {
@@ -21,10 +21,13 @@ def postCleanup() {
 def upstreamToECR() {
   if (params.IS_DEPLOYING) {
     env.CURRENT_HASH = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
+    sh "docker context use default"
     sh 'aws ecr get-login-password --region $ECR_REGION --profile joshua | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$ECR_REGION.amazonaws.com'
     sh "docker build -t ${DOCKER_IMAGE} ."
-    sh 'docker tag $DOCKER_IMAGE:latest $AWS_ACCOUNT_ID.dkr.ecr.$ECR_REGION.amazonaws.com/$DOCKER_IMAGE-jd:$CURRENT_HASH'
-    sh 'docker push $AWS_ACCOUNT_ID.dkr.ecr.$ECR_REGION.amazonaws.com/$DOCKER_IMAGE-jd:$CURRENT_HASH'
+    sh 'docker tag $DOCKER_IMAGE:latest $AWS_ACCOUNT_ID.dkr.ecr.$ECR_REGION.amazonaws.com/$DOCKER_IMAGE-microservice-jd:$CURRENT_HASH'
+    sh 'docker tag $DOCKER_IMAGE:latest $AWS_ACCOUNT_ID.dkr.ecr.$ECR_REGION.amazonaws.com/$DOCKER_IMAGE-microservice-jd:latest'
+    sh 'docker push $AWS_ACCOUNT_ID.dkr.ecr.$ECR_REGION.amazonaws.com/$DOCKER_IMAGE-microservice-jd:$CURRENT_HASH'
+    sh 'docker push $AWS_ACCOUNT_ID.dkr.ecr.$ECR_REGION.amazonaws.com/$DOCKER_IMAGE-microservice-jd:latest'
   }
 }
 
